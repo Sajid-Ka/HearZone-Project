5ac700f0d4a78258277a4ee664b663906612da46
Sajid.K.A <sajidkanjirathingal609@gmail.com>
2025-02-18 18:36:45 +0530

    Schema adding

---
+++ models/categorySchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    isListed: {
+        type: Boolean,
+        default: true,
+    },
+    categoryOffer: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
